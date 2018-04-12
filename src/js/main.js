3bc999016756760e6bfab04cf5284016728de954
Purcelea Iulian Valentin <p.iulian.valentin@outlook.com>
2018-04-12 17:12:55 +0300

    Last commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,7 @@ var $mobileButton = $(".navbar-toggler");
 var $clientText = $(".client-text");
 var $clientWrapper = $clientText.find(".client-text-wrapper");
 var $clientTrigger = $clientText.find(".client-text-trigger");
-
 var $clientSlider = $(".client-image-slider");
-var $clientOverlayRed = $clientSlider.find(".clients-overlay-red");
-var $clientOverlayGrey = $clientSlider.find(".clients-overlay-grey");
 var activeClient = 0;
 
 $mobileButton.click(navbarAnimation);
@@ -15,13 +12,13 @@ $clientTrigger.click(clientAnimation);
 function clientAnimation() {
 	let margin = $clientTrigger.index($(this));
 	activeClient = margin;
-	$clientOverlayRed.removeClass("clients-overlay-1 clients-overlay-2 clients-overlay-0");
-	$clientOverlayGrey.removeClass("clients-overlay-1 clients-overlay-2 clients-overlay-0")
-	$clientOverlayRed.addClass("clients-overlay-" + ( (margin + 1) % 3));
-	$clientOverlayGrey.addClass("clients-overlay-" + ( (margin + 2) % 3));
+	$clientSlider.children().removeClass("client-image-overlay-red").removeClass("client-image-overlay-grey").removeClass("client-image-scale");
+	$clientSlider.children().eq(margin).addClass("client-image-scale");
+	$clientSlider.children().eq((margin + 1) % 3).addClass("client-image-overlay-red");
+	$clientSlider.children().eq((margin + 2) % 3).addClass("client-image-overlay-grey");
 	$clientTrigger.removeClass("trigger-active");
 	$(this).addClass("trigger-active");
-	$clientWrapper.animate({"margin-left" : "-380" * margin}, 400);
+	$clientWrapper.animate({"margin-left" : "-480" * margin}, 400);
 }
 
 
